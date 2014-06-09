7aad4af379a0b85c7a06404ae0f9d0e57798fedb
Brian.Schmitt <brianschmitt@gmail.com>
2014-06-09 10:01:17 -0400

    Adding git powershell scripts
    A script file to hold various powershell helper routines for git

---
+++ git.ps1
@@ -0,0 +1,15 @@
+﻿# Requires PoshGit to work properly https://github.com/dahlbyk/posh-git
+function Find-Unpushed {
+    Push-Location
+    $folders = Get-ChildItem -Directory -Filter .git -Hidden -Recurse -ErrorAction SilentlyContinue
+    foreach ($folder in $folders) {
+        Set-Location $folder.Parent.FullName
+        $status = Get-GitStatus
+        if ($status.AheadBy -ne 0 -or $status.BehindBy -ne 0) {
+            Write-Host $folder.Parent.FullName -NoNewline
+            Write-GitStatus $status
+            Write-Host
+        }
+    }
+    Pop-Location
+}
