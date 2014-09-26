95e036c507939a0a27979834a8498b57d51b0891
Brian.Schmitt <brianschmitt@gmail.com>
2014-09-26 15:45:02 -0400

    Added a Pub file check and prompt to download if missing

--- azure.ps1
+++ azure.ps1
@@ -2,18 +2,23 @@
 # Basic wrappers around the Azure powershell commands
 # This will attempt to locate a VM, you just need to supply enough information to uniquely locate
 # Then it will Start/Stop the instance
-Import-AzurePublishSettingsFile '~\.ssh\azure.publishsettings'
+if (Test-Path '~\.ssh\azure.publishsettings') {
+    Import-AzurePublishSettingsFile '~\.ssh\azure.publishsettings'
+} else {
+    Write-Output 'Missing the publish file, please download and save'
+    Get-AzurePublishSettingsFile
+}
 
 function Start-Azure {
     Param([Parameter(Mandatory=$true, HelpMessage="Enter a VM name")] $VirtualMachineName)
     $azurevm = Get-AzureVM | Where-Object {$_.Name -like $VirtualMachineName + "*"} | select ServiceName, Name
-    Write-Host Starting $azurevm.Servicename
+    Write-Output Starting $azurevm.Servicename
 	Start-AzureVM -ServiceName $azurevm.servicename -Name $azurevm.name
 }
 
 function Stop-Azure {
     Param([Parameter(Mandatory=$true, HelpMessage="Enter a VM name")] $VirtualMachineName)
     $azurevm = Get-AzureVM | Where-Object {$_.Name -like $VirtualMachineName + "*"} | select servicename, Name
-    Write-Host Stopping $azurevm.Servicename
+    Write-Output Stopping $azurevm.Servicename
 	Stop-AzureVM -ServiceName $azurevm.servicename -Name $azurevm.name -Force
 }
