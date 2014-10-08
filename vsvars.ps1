cae8d4188f7cb1ea912d7e739ea71be1f6b95a07
Brian.Schmitt <brianschmitt@gmail.com>
2014-10-08 09:24:51 -0400

    updated to handle if VS is installed

--- vsvars.ps1
+++ vsvars.ps1
@@ -2,13 +2,19 @@
 # Set environment variables for Visual Studio Command Prompt
 # http://stackoverflow.com/questions/2124753/how-i-can-use-powershell-with-the-visual-studio-command-prompt
 #
- 
-pushd 'c:\Program Files (x86)\Microsoft Visual Studio 11.0\VC'
-cmd /c "vcvarsall.bat&set" |
-foreach {
-  if ($_ -match "=") {
-    $v = $_.split("="); set-item -force -path "ENV:\$($v[0])"  -value "$($v[1])"
-  }
+
+$VSVarsPath = 'c:\Program Files (x86)\Microsoft Visual Studio 11.0\VC'
+
+if (Test-Path $VSVarsPath) {
+    Push-Location $VSVarsPath
+    cmd /c "vcvarsall.bat&set" |
+    foreach {
+      if ($_ -match "=") {
+        $v = $_.split("="); set-item -force -path "ENV:\$($v[0])"  -value "$($v[1])"
+      }
+    }
+    Pop-Location
+    Write-Host "`nVisual Studio Command Prompt variables set." -ForegroundColor DarkBlue
+} else {
+    Write-Host "Visual Studio not installed or not found" -ForegroundColor Red
 }
-popd
-write-host "`nVisual Studio Command Prompt variables set." -ForegroundColor DarkBlue
