c056db260f09cf1ee1e7c999e796b366c6654d5c
Brian.Schmitt <brianschmitt@gmail.com>
2014-10-08 11:07:02 -0400

    updated to support path parameter

--- git.ps1
+++ git.ps1
@@ -1,15 +1,15 @@
-﻿# Requires PoshGit to work properly https://github.com/dahlbyk/posh-git
+﻿# Requires my version of PoshGit to work properly https://github.com/brianschmitt/posh-git
+# this version supports passing a folder to the various commands
 function Find-Unpushed {
-    Push-Location
     $folders = Get-ChildItem -Directory -Filter .git -Hidden -Recurse -ErrorAction SilentlyContinue
     foreach ($folder in $folders) {
-        Set-Location $folder.Parent.FullName
-        $status = Get-GitStatus
-        if ($status.AheadBy -ne 0 -or $status.BehindBy -ne 0) {
+        $status = Get-GitStatus $folder.Parent.FullName
+        if (($status.HasWorking -and ($status.Working.AheadBy -ne 0 -or $status.Working.BehindBy -ne 0)) -or
+                ($status.HasIndex -and ($status.Index.AheadBy -ne 0 -or $status.Index.BehindBy -ne 0)) -or
+                ($status.AheadBy -ne 0 -or $status.BehindBy -ne 0)) {
             Write-Host $folder.Parent.FullName -NoNewline
             Write-GitStatus $status
             Write-Host
         }
     }
-    Pop-Location
 }
