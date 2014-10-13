132ec30bb3fdf2f4158039a3d418f2f37ceee0f7
Brian.Schmitt <brianschmitt@gmail.com>
2014-10-13 11:15:50 -0400

    Update order of execution

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,11 +1,5 @@
 $scriptRoot = Split-Path (Resolve-Path $myInvocation.MyCommand.Path)
 
-Import-Module PSReadline
-Import-Module (join-path $scriptRoot "/posh-git/posh-git.psm1")
-Enable-GitColors
-Start-SshAgent -Quiet
-Import-Module (join-path $scriptRoot "/posh-tf/posh-tf.psm1")
-
 . (join-path $scriptRoot "/git.ps1") # Helper functions for git
 . (join-path $scriptRoot "/azure.ps1") # Helper functions for VM instances
 . (join-path $scriptRoot "/vsvars.ps1") # Set environment variables for Visual Studio Command Prompt
@@ -13,6 +7,14 @@ Import-Module (join-path $scriptRoot "/posh-tf/posh-tf.psm1")
 . (join-path $scriptRoot "/directory.ps1") # Helper functions to assist with directory list commands
 . (join-path $scriptRoot "/apci/apci.ps1") # Helper functions to assist with AP commands
 . (join-path $scriptRoot "/activedirectory.ps1") # Helper functions to assist with AD commands
+
+Import-Module PSReadline
+Import-Module (join-path $scriptRoot "/posh-git/posh-git.psm1")
+Enable-GitColors
+Start-SshAgent -Quiet
+Import-Module (join-path $scriptRoot "/posh-tf/posh-tf.psm1")
+#$TFPromptSettings.EnableServerStatus = $false
+#$TFPromptSettings.Debug = $true
 
 # Type ver to get version information...
 function Get-PSVersion {
