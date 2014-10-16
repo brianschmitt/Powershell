15d46a74c3a30b7aa891cfc1cda7598c787b1e6c
Brian.Schmitt <brianschmitt@gmail.com>
2014-10-16 12:55:34 -0400

    Add function to load Publish file
    Loading the file on every terminal start caused slowness
    These functions aren't used very often, so we will
    explicity load them as necessary.

--- azure.ps1
+++ azure.ps1
@@ -2,14 +2,17 @@
 # Basic wrappers around the Azure powershell commands
 # This will attempt to locate a VM, you just need to supply enough information to uniquely locate
 # Then it will Start/Stop the instance
-if (Test-Path '~\.ssh\azure.publishsettings') {
-    Import-AzurePublishSettingsFile '~\.ssh\azure.publishsettings'
-} else {
-    Write-Warning 'Missing the publish file, please download and save - Run "Get-AzurePublishSettingsFile"'
+function Load-AzurePublish {
+    if (Test-Path '~\.ssh\azure.publishsettings') {
+        Import-AzurePublishSettingsFile '~\.ssh\azure.publishsettings'
+    } else {
+        Write-Warning 'Missing the publish file, please download and save - Run "Get-AzurePublishSettingsFile"'
+    }
 }
 
 function Start-Azure {
     Param([Parameter(Mandatory=$true, HelpMessage="Enter a VM name")] $VirtualMachineName)
+    Load-AzurePublish
     $azurevm = Get-AzureVM | Where-Object {$_.Name -like $VirtualMachineName + "*"} | select ServiceName, Name
     Write-Output Starting $azurevm.Servicename
 	Start-AzureVM -ServiceName $azurevm.servicename -Name $azurevm.name
@@ -17,6 +20,7 @@ function Start-Azure {
 
 function Stop-Azure {
     Param([Parameter(Mandatory=$true, HelpMessage="Enter a VM name")] $VirtualMachineName)
+    Load-AzurePublish
     $azurevm = Get-AzureVM | Where-Object {$_.Name -like $VirtualMachineName + "*"} | select servicename, Name
     Write-Output Stopping $azurevm.Servicename
 	Stop-AzureVM -ServiceName $azurevm.servicename -Name $azurevm.name -Force
