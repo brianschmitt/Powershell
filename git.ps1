02d18aa3fcabcc43f30dd8d658023331b05e6a52
Brian.Schmitt <brianschmitt@gmail.com>
2014-10-16 12:57:13 -0400

    Refactored slightly to select directory paths

--- git.ps1
+++ git.ps1
@@ -1,13 +1,11 @@
 ﻿# Requires my version of PoshGit to work properly https://github.com/brianschmitt/posh-git
 # this version supports passing a folder to the various commands
 function Find-Unpushed {
-    $folders = Get-ChildItem -Directory -Filter .git -Hidden -Recurse -ErrorAction SilentlyContinue
+    $folders = Get-ChildItem -Directory -Filter .git -Hidden -Recurse -ErrorAction SilentlyContinue | select -expand parent | select -ExpandProperty fullname
     foreach ($folder in $folders) {
-        $status = Get-GitStatus $folder.Parent.FullName
-        if (($status.HasWorking -and ($status.Working.AheadBy -ne 0 -or $status.Working.BehindBy -ne 0)) -or
-                ($status.HasIndex -and ($status.Index.AheadBy -ne 0 -or $status.Index.BehindBy -ne 0)) -or
-                ($status.AheadBy -ne 0 -or $status.BehindBy -ne 0)) {
-            Write-Host $folder.Parent.FullName -NoNewline
+        $status = Get-GitStatus $folder
+        if ($status.HasWorking -or $status.HasIndex -or $status.AheadBy -ne 0 -or $status.BehindBy -ne 0) {
+            Write-Host $folder -NoNewline
             Write-GitStatus $status
             Write-Host
         }
