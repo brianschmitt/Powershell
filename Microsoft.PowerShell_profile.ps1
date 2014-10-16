77e51b7e55b0974c152d05b7e16756c06ffec7d8
Brian.Schmitt <brianschmitt@gmail.com>
2014-10-16 12:59:06 -0400

    Use builtin ScriptRoot

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,24 +1,21 @@
-$scriptRoot = Split-Path (Resolve-Path $myInvocation.MyCommand.Path)
-
-. (join-path $scriptRoot "/git.ps1") # Helper functions for git
-. (join-path $scriptRoot "/azure.ps1") # Helper functions for VM instances
-. (join-path $scriptRoot "/vsvars.ps1") # Set environment variables for Visual Studio Command Prompt
-. (join-path $scriptRoot "/tfs.ps1") # Helper functions for TFS
-. (join-path $scriptRoot "/directory.ps1") # Helper functions to assist with directory list commands
-. (join-path $scriptRoot "/apci/apci.ps1") # Helper functions to assist with AP commands
-. (join-path $scriptRoot "/activedirectory.ps1") # Helper functions to assist with AD commands
+. (join-path $PSScriptRoot "/git.ps1") # Helper functions for git
+. (join-path $PSScriptRoot "/azure.ps1") # Helper functions for VM instances
+. (join-path $PSScriptRoot "/vsvars.ps1") # Set environment variables for Visual Studio Command Prompt
+. (join-path $PSScriptRoot "/tfs.ps1") # Helper functions for TFS
+. (join-path $PSScriptRoot "/directory.ps1") # Helper functions to assist with directory list commands
+. (join-path $PSScriptRoot "/apci/apci.ps1") # Helper functions to assist with AP commands
+. (join-path $PSScriptRoot "/activedirectory.ps1") # Helper functions to assist with AD commands
 
 Import-Module PSReadline
-Import-Module (join-path $scriptRoot "/posh-git/posh-git.psm1")
+Import-Module (join-path $PSScriptRoot "/posh-git/posh-git.psm1")
 Enable-GitColors
 Start-SshAgent -Quiet
-Import-Module (join-path $scriptRoot "/posh-tf/posh-tf.psm1")
+Import-Module (join-path $PSScriptRoot "/posh-tf/posh-tf.psm1")
 
-# Type ver to get version information...
 function Get-PSVersion {
     Write-Output $PSVersionTable
 }
-Set-Alias ver Get-PSVersion
+Set-Alias ver Get-PSVersion # Type ver to get version information...
 
 function prompt {
     $userLocation = $env:username + '@' + [System.Environment]::MachineName
@@ -28,6 +25,5 @@ function prompt {
     Write-Host('>') -NoNewline
     return " "
 }
-
 
 Set-Item "ENV:\GREP_OPTIONS" -value "--color=auto --exclude-dir=.git"
