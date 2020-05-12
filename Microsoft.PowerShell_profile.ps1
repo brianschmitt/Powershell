04b2d1a12d6c1dfb061e23bdcf3618fcc153dac9
Brian Schmitt <brianschmitt@gmail.com>
2020-05-12 10:58:08 -0400

    Code line counter

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -45,3 +45,19 @@ function Get-PSVersion {
     Write-Output  -InputObject $PSVersionTable
 }
 Set-Alias  -Name ver  -Value Get-PSVersion # Type ver to get version information...
+
+function Count-CodeLines {
+    Get-ChildItem -Recurse ($source) -Include *.cs -File |
+    Where-Object { $_.PSParentPath -notmatch "obj|bin|Resources" } | 
+    ForEach-Object { (Get-Content $_).Count } | 
+    Measure-Object -Sum
+}
+
+function Show-LineCount {
+    Get-ChildItem -Recurse ($source) -Include *.cs -File |
+    Where-Object { $_.PSParentPath -notmatch "obj|bin|Resources" } | 
+    ForEach-Object { [PSCustomObject] @{
+            FileName  = $_
+            LineCount = (Get-Content $_).Count
+        } }
+}
